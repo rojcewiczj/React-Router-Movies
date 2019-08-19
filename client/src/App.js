0bb7884afcc421934e0e3f4ffdae91b84e8ec383
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-19 14:45:30 -0500

    added imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import ReactDOM from "react-dom";
 
+import { Route } from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
@@ -11,10 +14,14 @@ const App = () => {
 
   return (
     <div>
+      <Router>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      </Router>
     </div>
   );
 };
 
 export default App;
+const rootElment = document.getElementById('root');
+ReactDOM.render (<Router><App /></Router>, rootElment);
